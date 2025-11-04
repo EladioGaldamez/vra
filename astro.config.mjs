d92bfee9a63bf06a8576be0879b54ee2a46f74a9
José Galdámez <eladio.galdamez@gmail.com>
2025-11-03 20:35:25 -0600

    sitemap + robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,21 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 // import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   // output: "server",
   // adapter: netlify(),
+  site: "https://www.tinarosero.com",
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes("/success"),
+    }),
+  ],
 });
